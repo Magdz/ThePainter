0b24c2c809ea12c69b5e4a8521ad8f77f08fd93f
Dina-Nashaat <dina.nashaat@gmail.com>
2015-12-10 18:52:05 +0200

    Edit Layer Style

--- src/Views/LayerStyle.java
+++ src/Views/LayerStyle.java
@@ -6,12 +6,21 @@
 package Views;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Shape;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 /**
  *
@@ -19,22 +28,40 @@ import javax.swing.JPanel;
  */
 public class LayerStyle extends JPanel {
 
-    final private JPanel container = new JPanel();
-    final private JLabel label = new JLabel();
+    private JPanel panel = new JPanel();
+    
+    private Container container = new Container();
+
+    public LayerStyle(Container container) {
+        this.container = container;
+    }
+    
+    public void addComp ()
+    {
+        container.setLayout(new BoxLayout(container,BoxLayout.Y_AXIS));
+
+        addLabel("Layer1",container);
+        addLabel("Layer2",container);
+
+    }
+    
+    private void addLabel(String text, Container container) {
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel();
+        label.setText(text);
+        panel.add(label);
+
+        panel.setBorder(BorderFactory.createLoweredBevelBorder());
+        panel.setMaximumSize(new Dimension(Integer.MAX_VALUE,panel.getMinimumSize().height));
+
+        container.add(panel);
+    }
     
     //Needs testing
-   /* protected void paintComponent(Shape s, Graphics2D g2d) {
+
+    /* protected void paintComponent(Shape s, Graphics2D g2d) {
         super.paintComponent(g2d);
         g2d.draw(s);
     }
-    */
-    public void setContainer(String text, JPanel panel){
-        this.label.setText(text);
-        container.add(label);
-        panel.add(container);
-        container.setSize(230,30);
-        label.setSize(220,20);
-        container.setBorder(BorderFactory.createEtchedBorder(Color.black, Color.lightGray));
-        
-    }
+     */
 }
