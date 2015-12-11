056cc03df14368b0cbc9719dfda506fb916fa965
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-12-11 16:44:50 +0200

    Draw Square

--- src/Models/Shapes/Square.java
+++ src/Models/Shapes/Square.java
@@ -4,10 +4,12 @@ import java.awt.Color;
 
 public class Square extends Rectangle {
 	
+    private double length;
 
-	public Square(double x1, double y1, double x2 , double y2, Color color) {
-		super(x1, y1, x2, y2, color); //length=width
-		this.width = this.height;
-	}
-	
+    public Square(double x1, double y1, double x2 , double y2, Color color) {
+            super(x1, y1, x2, y2, color); //length=width
+            length = Math.max(super.width,super.height);
+            super.width = super.height = length;
+    }
+    
 }
