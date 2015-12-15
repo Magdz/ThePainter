d55650df9c899f4b5986cecc65b649f8e35df07d
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-12-15 12:11:28 +0200

    Load basic shapes from XML

--- src/Controllers/SaveLoad.java
+++ src/Controllers/SaveLoad.java
@@ -6,6 +6,9 @@
 package Controllers;
 
 import Models.Layers.Canvas;
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.BufferedInputStream;
@@ -15,8 +18,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.function.Consumer;
 import javax.swing.JFileChooser;
-import models.shapes.Shape;
+import models.shapes.*;
 
 /**
  *
@@ -38,13 +42,14 @@ public class SaveLoad {
     
     public void Load(String Format,Canvas canvas){
         this.canvas = canvas;
-        this.shapes = null;
+        this.shapes = new ArrayList();
         try{
             if(Format.equalsIgnoreCase("xml"))decodeObjectFromXML();
         }catch(Exception e){
             System.out.println("Loading Failed");
         }
         this.canvas.setAllShapes(this.shapes);
+        this.canvas.repaint();
     }
     
     private void encodeObjectToXML() throws FileNotFoundException{
@@ -58,14 +63,17 @@ public class SaveLoad {
             } catch (Exception ex) {}
         }
         try (XMLEncoder XMLEncoder = new XMLEncoder(new BufferedOutputStream(fOutput))) {
-            shapes.stream().forEach((shape) -> {
-                XMLEncoder.writeObject(shape.getShape());
+            shapes.stream().forEach(new Consumer<Shape>() {
+                @Override
+                public void accept(Shape shape) {
+                    XMLEncoder.writeObject(shape.getShape());
+                    XMLEncoder.writeObject(shape.getColor());
+                }
             });
         }
     }
     
     private void decodeObjectFromXML() throws FileNotFoundException{
-
         JFileChooser FileChooser = new JFileChooser();
         FileInputStream fInput = null;
         FileChooser.setCurrentDirectory(new File("/"));
@@ -76,7 +84,16 @@ public class SaveLoad {
             } catch (Exception ex) {}
         }
         try (XMLDecoder XMLDecoder = new XMLDecoder(new BufferedInputStream((fInput)))) {
-            shapes = (ArrayList<Shape>)XMLDecoder.readObject();
+            Object loaded = XMLDecoder.readObject();
+            if(loaded instanceof Rectangle2D.Double){
+                Rectangle2D.Double Rectangle = (Rectangle2D.Double) loaded;
+                loaded = XMLDecoder.readObject();
+                shapes.add(new Rectangle(Rectangle.x,Rectangle.y,Rectangle.x+Rectangle.width,Rectangle.y+Rectangle.height,(Color)loaded));
+            }else if(loaded instanceof Ellipse2D.Double){
+                Ellipse2D.Double Ellipse = (Ellipse2D.Double) loaded;
+                loaded = XMLDecoder.readObject();
+                shapes.add(new Elipse((Color)loaded, Color.yellow, 5, Ellipse.x, Ellipse.y, Ellipse.height, Ellipse.width));
+            }
         }
     }
     
